dfa6c18d431e8c40b3705e87589658725f61ebcb
postog78 <postog@mail.ru>
2016-09-27 20:16:54 +0300

    error visit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,20 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_url
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+  
 end
 
 class ApplicationController < ActionController::Base
