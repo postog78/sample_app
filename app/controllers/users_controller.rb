faf75a4644592b35746c8ce9e6b21f5a2c7c38d7
postog78 <postog@mail.ru>
2016-10-03 23:58:22 +0300

    Перед тем как не крутится окно

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-   def show
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def show
     @user = User.find(params[:id])
   end
   
@@ -19,11 +26,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
   def update
-     @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -31,15 +38,25 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
-   def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-   end
-    
+      
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
